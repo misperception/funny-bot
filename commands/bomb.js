d7cffb591c4828429ad14ed5e332e6917110b110
misperception <victoriuscamver@gmail.com>
2021-08-24 00:13:07 +0200

    changed the zip bomb's name

--- commands/bomb.js
+++ commands/bomb.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "Sends a zip bomb when requested",
     execute(message, args){
         const Discord = require("discord.js");
-        const attachment = new Discord.MessageAttachment("./trabajo.zip");
+        const attachment = new Discord.MessageAttachment("./pog.zip");
         message.channel.send("here you go retard", attachment);
     }
 }
