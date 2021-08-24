f08b8143ac40ba5392b42dba5e0d7cc12e2de9f0
misperception <victoriuscamver@gmail.com>
2021-08-24 22:48:56 +0200

    updated ratio command

--- commands/ratio.js
+++ commands/ratio.js
@@ -1,10 +1,10 @@
 const { MessageEmbed, Channel } = require("discord.js")
-
 module.exports = {
     name: "ratio",
     description: "0-100 rating of your retardation",
     execute(message,args){
         var retard = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
+        const avatar = message.author.avatarURL();
         const embed = new MessageEmbed()
             .setTitle("**You are " + retard + "% retarded**");
             if(retard <= 20 ){
@@ -23,14 +23,15 @@ module.exports = {
                 embed.setColor("DARK_RED")
             }
             if(retard == 0){
-                embed.setFooter("gg bro");
+                embed.setFooter("gg bro", avatar);
             }
             else if(retard == 100){
-                embed.setFooter("kys");
+                embed.setFooter("kys", avatar);
             }
             else{
-                embed.setFooter("This is " + message.author.tag + "'s retard rate.")
+                embed.setFooter("This is " + message.author.tag + "'s retard rate.", avatar)
             }
+            console.log(avatar);
         message.channel.send(embed);
     }
 }   
